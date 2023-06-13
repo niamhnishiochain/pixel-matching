29235e8d8297e522b3ac0b65c9bc237e612a65cc
Niamh Sheehan <sheehan3@tcd.ie>
2023-06-13 21:06:10 +0200

    Update matching.py
    1. Full buffer tiff (only selected bands)
    2. Sample from Sarara area
    3. Optimise the faiss implementation for speed vs accuracy trade off

--- matching.py
+++ matching.py
@@ -3,7 +3,10 @@ import rasterio
 import numpy as np 
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
-    
+from datetime import datetime
+
+np.random.seed(42)
+ 
 # Open the GeoTIFF file using rasterio and preprocess it 
 def read_geotiff_to_array(file_path):
     """
@@ -18,7 +21,7 @@ def read_geotiff_to_array(file_path):
     """
     with rasterio.open(file_path) as dataset:
     # Read all the bands into separate NumPy arrays
-        band_data = {band: dataset.read(i + 1) for i, band in enumerate(dataset.indexes)}
+        band_data = {band: dataset.read(i   + 1) for i, band in enumerate(dataset.indexes)}
     # Reshape the band data arrays into 1D arrays
     reshaped_data = {band: data.flatten() for band, data in band_data.items()}
     # Create a pandas DataFrame from the reshaped band data dictionary
@@ -32,25 +35,47 @@ def read_geotiff_to_array(file_path):
     return scaled
 
 #Project Area
-sarara_file_path = r'C:\Users\35387\Downloads\trialTiff.tif'
+sarara_file_path = r'C:\Users\35387\OneDrive\Documents\learning\sarara.tif'
 sarara_array = read_geotiff_to_array(sarara_file_path)
 
+#Randomly sample 1% from the Sarara array
+# Calculate the number of elements for the 1% sample
+sample_size = round(len(sarara_array) * 0.01)
+print('sample size', sample_size)
+# Randomly sample 
+sarara_sample = np.random.choice(sarara_array.flatten(), size=sample_size, replace=False)
+sarara_sample = sarara_sample.reshape(-1, sarara_array.shape[1])
+
 #Buffer Area
-buffer_file_path = r'C:\Users\35387\Downloads\bufferSub.tif'
+buffer_file_path = r'C:\Users\35387\OneDrive\Documents\learning\band_subset_buffer.tif'
 buffer_array = read_geotiff_to_array(buffer_file_path)
 
 #FAISS Set Up
+#faiss.write_index(index, "populated.index")
+index = faiss.read_index("populated.index")
+"""
+#Train and add to the index
+nlist= 100                     #number of Voronoi cells
 d = sarara_array.shape[1]      #dimensions
-index = faiss.IndexFlatL2(d)   #build the index
-print(index.is_trained)
-index.add(buffer_array)        #add vectors to the index
-print(index.ntotal)
+quantizer = faiss.IndexFlatL2(d)  #the other index
+index = faiss.IndexIVFFlat(quantizer, d, nlist)
+assert not index.is_trained
+index.train(buffer_array)   
+assert index.is_trained
 
-k = 3                         # we want to see X nearest neighbors
-D, I = index.search(sarara_array[:5], k) # sanity check - want the distances to increase
+index.add(buffer_array)        #add vectors to the index
+print(index.ntotal)            #number of pixels indexed
+"""
+#Search Subsection
+k = 1                         # we want to see k nearest neighbors
+index.nprobe = 10             #number of nearby cells to search
+D, I = index.search(sarara_sample[:5], k) # sanity check - want the distances to increase
 print(I)                      # IDs
 print(D)                      # Distances
 
-D, I = index.search(sarara_array, k)     # actual search
+#Full Search  
+startTime = datetime.now()
+D, I = index.search(sarara_sample, k)     # actual search
 print(I[:5])                   # neighbors of the 5 first queries
 print(I[-5:])                  # neighbors of the 5 last queries
+print(datetime.now() - startTime)
