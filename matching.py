90342cf5048a80d35ae40ee8325cb94685c03675
Niamh Sheehan <sheehan3@tcd.ie>
2023-06-23 16:24:16 +0200

    Sample to Coordinates Working
    Outstanding: Buffer memory issue using same approach

--- matching.py
+++ matching.py
@@ -53,7 +53,7 @@ def read_geotiff_to_array_faiss(file_path, all_bands: True):
     return array
 
 # Open the GeoTIFF file using rasterio and preprocess to analyse matching results
-def read_geotiff_to_array_results(file_path, number_of_bands: 17, select_indices):
+def read_geotiff_to_array_results(file_path, select_indices):
     """
     Purpose
     ----------
@@ -77,6 +77,7 @@ def read_geotiff_to_array_results(file_path, number_of_bands: 17, select_indices
         band_data = {band: dataset.read(i   + 1) for i, band in enumerate(dataset.indexes)}        
     # Reshape the band data arrays into 1D arrays
     reshaped_data = {band: data.flatten() for band, data in band_data.items()}
+    array = np.transpose(np.array(list(reshaped_data.values())))
     #add coordinate information
     x_coords = []
     y_coords = []
@@ -85,14 +86,21 @@ def read_geotiff_to_array_results(file_path, number_of_bands: 17, select_indices
             x, y = dataset.xy(row, col)
             x_coords.append(x)
             y_coords.append(y)
-    next_to_last = number_of_bands + 1
-    last = number_of_bands + 2
-    output = {**reshaped_data, next_to_last: x_coords, last: y_coords}
-    #reshape
-    array = np.transpose(np.array(list(output.values())))
+    array = np.c_[array, x_coords]
+    array = np.c_[array, y_coords]
     
+    array[array == -999] = np.nan
+    array = array[~np.any(np.isnan(array), axis=1)]   
+    # Scale the values
+    scaler = MinMaxScaler()
+    # transform data
+    if all_bands == True:
+        array[:, 0:17] = scaler.fit_transform(array[:, 0:17])
+    else:
+        
     #subset to the matches or sample 
-    array = array[select_indices]
+    array = array[select_indices, :]
+
     return array
 
 #%%
@@ -116,36 +124,21 @@ buffer_file_path = r'C:\Users\35387\OneDrive\Documents\learning\data\earth_engin
 buffer_array = read_geotiff_to_array_faiss(buffer_file_path, False)
 
 #%%
-d = sarara_array.shape[1]
-index = faiss.IndexFlatL2(d)   # build the index
-print(index.is_trained)
-index.add(buffer_array)                  # add vectors to the index
-print(index.ntotal)
-
-#%%
-k = 5                        # we want to see 4 nearest neighbors
-D, I = index.search(sarara_sample[:5], k) # sanity check
-print(I)
-print(D)
-
-
-#%%
 #FAISS Set Up
 #faiss.write_index(index, "populated.index")
-#index = faiss.read_index("populated.index")
-           #number of nearby cells to search
-
+index = faiss.read_index("populated.index")
+k = 1                         # we want to see k nearest neighbors   
+"""    
 #Train and add to the index
 nlist= 100                     #number of Voronoi cells
-d = 1 #sarara_array.shape[1]      #dimensions
+d = sarara_array.shape[1]      #dimensions
 quantizer = faiss.IndexFlatL2(d)  #the other index
 index = faiss.IndexIVFFlat(quantizer, d, nlist)
 assert not index.is_trained
 index.train(buffer_array)   
 assert index.is_trained
 
-k = 1                         # we want to see k nearest neighbors
-index.nprobe = 10             # number of Voronoi cells to search
+index.nprobe = 10             # number nearby of Voronoi cells to search
 index.add(buffer_array)        #add vectors to the index
 print(index.ntotal)            #number of pixels indexed
 #faiss.write_index(index, "populated.index") #save the final index to disk
@@ -154,28 +147,70 @@ print(index.ntotal)            #number of pixels indexed
 D, I = index.search(sarara_sample[:5], k) # sanity check - want the distances to increase
 print(I)                      # IDs
 print(D)                      # Distances
-
+"""
 #%%
 #Full Search  
 startTime = datetime.now()
-D, I = index.search(sarara_sample[:, 0:4], k)     # actual search
+D, I = index.search(sarara_sample, k)     # actual search
 print(I[:5])                   # neighbors of the 5 first queries
 print(I[-5:])                  # neighbors of the 5 last queries
 print(datetime.now() - startTime)
 
 #%%
 #Results export and explore
+sarara_sample_matches = read_geotiff_to_array_results(sarara_file_path, sample_indices)
+buffer_matches = read_geotiff_to_array_results(buffer_file_path, I.flatten())
+
+#%%
+#Sense checking result outputs
+def check_column_values_match(array1, array2, columns1, columns2, num_rows=5):
+    """
+    Check if the values in specified columns of two NumPy arrays match for the first 'num_rows' rows.
+
+    Args:
+        array1 (np.ndarray): First NumPy array.
+        array2 (np.ndarray): Second NumPy array.
+        columns1 (list): List of column indices for array1.
+        columns2 (list): List of column indices for array2.
+        num_rows (int): Number of rows to compare (default: 5).
+
+    Returns:
+        bool: True if values match for all specified columns and rows, False otherwise.
+    """
+    if array1.shape[0] < num_rows or array2.shape[0] < num_rows:
+        raise ValueError("Number of rows in the arrays is less than 'num_rows'.")
+
+    if len(columns1) != len(columns2):
+        raise ValueError("Number of columns in arrays is not equal.")
+
+    array1_subset = array1[:num_rows, columns1]
+    array2_subset = array2[:num_rows, columns2]
+
+    return np.array_equal(array1_subset, array2_subset)
+
+
+#
+sample_columns = [0, 1, 2, 3]
+match_columns = [10, 12, 13, 14]
+
+match = check_column_values_match(sarara_sample, sarara_sample_matches, sample_columns, match_columns)
+print("Values match for the specified columns and rows:", match)
+
+
+#%%
 #add the match id to the project area sample array 
 sarara_sample_matched = np.concatenate((sarara_sample, I), axis = 1)
 
 buffer_file_path = r'C:\Users\35387\OneDrive\Documents\learning\band_subset_buffer.tif'
-dataset = rasterio.open(buffer_file_path)
+dataset = rasterio.open(sarara_file_path)
     
 buffer_array[list(I.flatten())]
 
 matching_buffer_coords = np.column_stack((buffer_array[list(I.flatten())], np.array(match_coords_x)))
 matching_buffer_coords = np.column_stack((matching_buffer_coords, np.array(match_coords_y)))
 
+#%%
+#sample_matches_df.to_csv('sample_matches_plot.csv', index = False)
 
 #SENSE CHECKING IN PLOT (geopands package issues so just export and plot outside venv)
 #X = pd.DataFrame(matching_buffer_coords, columns = ['distance_roads','distance_settlements','elevation', 'slope', 'x', 'y'])
