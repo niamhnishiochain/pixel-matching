6d80133870509c738f73d71e03e04530bff01b4e
Niamh Sheehan <sheehan3@tcd.ie>
2023-06-28 18:13:37 +0200

    Update matching.py
    Tidy up

--- python_scripts/matching.py
+++ python_scripts/matching.py
@@ -20,7 +20,7 @@ from datetime import datetime
 np.random.seed(42)
  
 # Open the GeoTIFF file using rasterio and preprocess it for FAISS execution
-def read_geotiff_to_array_faiss(file_path, all_bands: True):
+def read_geotiff_to_array_faiss(file_path):
     """
     Purpose
     ----------
@@ -48,15 +48,8 @@ def read_geotiff_to_array_faiss(file_path, all_bands: True):
     
     #Way of dealing with missing values (exporting from R we set NAs to -999)
         #there are so many missing values because raster always square matrix and the shape are not
-    array[array == -999] = np.nan
     array = array[~np.any(np.isnan(array), axis=1)]
 
-    #Way of dealing with large file sizes (just work with subset - 4 bands)
-    if all_bands == True:
-        to_keep = [10, 12,13, 14]
-        array = array[:, to_keep]
-    else:
-        pass
     # Scale the values
     scaler = MinMaxScaler()
     # transform data
@@ -110,8 +103,8 @@ def read_geotiff_to_array_results(file_path, select_indices):
 
 #%%
 #Project Area
-sarara_file_path = r'C:\Users\35387\OneDrive\Documents\learning\data\earth_engine_exports\sarara.tif'
-sarara_array = read_geotiff_to_array_faiss(sarara_file_path, True)
+sarara_file_path = r'C:\Users\35387\OneDrive\Documents\learning\earth_engine_export_data\matching\export_matching_data_sarara.tif'
+sarara_array = read_geotiff_to_array_faiss(sarara_file_path)
 #%%
 #Randomly sample 1% from the Sarara array
 # Calculate the number of elements for the 1% sample
@@ -125,15 +118,14 @@ sarara_sample = sarara_array[sample_indices]
 
 #%%
 #Buffer Area
-buffer_file_path = r'C:\Users\35387\OneDrive\Documents\learning\data\earth_engine_exports\buffer\band_subset_buffer_nan.tif'
-#buffer_array = read_geotiff_to_array_faiss(buffer_file_path, False)
+buffer_file_path = r'C:\Users\35387\OneDrive\Documents\learning\earth_engine_export_data\matching\export_matching_data_buffer.tif'
+buffer_array = read_geotiff_to_array_faiss(buffer_file_path)
 
 #%%
 #FAISS Set Up
-#faiss.write_index(index, "populated.index")
 index = faiss.read_index("populated.index")
 k = 1                         # we want to see k nearest neighbors   
-"""    
+"""  
 #Train and add to the index
 nlist= 100                     #number of Voronoi cells
 d = sarara_array.shape[1]      #dimensions
@@ -146,7 +138,7 @@ assert index.is_trained
 index.nprobe = 10             # number nearby of Voronoi cells to search
 index.add(buffer_array)        #add vectors to the index
 print(index.ntotal)            #number of pixels indexed
-#faiss.write_index(index, "populated.index") #save the final index to disk
+#faiss.write_index(index, "populated.index") #save the index to disk
 
 #Search subsection as sense check       
 D, I = index.search(sarara_sample[:5], k) # sanity check - want the distances to increase
@@ -169,55 +161,14 @@ sarara_sample_matches = read_geotiff_to_array_results(sarara_file_path, sample_i
 buffer_matches = read_geotiff_to_array_results(buffer_file_path, I.flatten())
 
 #%%
-#Sense checking result outputs
-def check_column_values_match(array1, array2, columns1, columns2, num_rows=5):
-    """
-    Check if the values in specified columns of two NumPy arrays match for the first 'num_rows' rows.
-
-    Args:
-        array1 (np.ndarray): First NumPy array.
-        array2 (np.ndarray): Second NumPy array.
-        columns1 (list): List of column indices for array1.
-        columns2 (list): List of column indices for array2.
-        num_rows (int): Number of rows to compare (default: 5).
-
-    Returns:
-        bool: True if values match for all specified columns and rows, False otherwise.
-    """
-    if array1.shape[0] < num_rows or array2.shape[0] < num_rows:
-        raise ValueError("Number of rows in the arrays is less than 'num_rows'.")
-
-    if len(columns1) != len(columns2):
-        raise ValueError("Number of columns in arrays is not equal.")
-
-    array1_subset = array1[:num_rows, columns1]
-    array2_subset = array2[:num_rows, columns2]
-
-    return np.array_equal(array1_subset, array2_subset)
-
-
-#
-sample_columns = [0, 1, 2, 3]
-match_columns = [10, 12, 13, 14]
-
-match = check_column_values_match(sarara_sample, sarara_sample_matches, sample_columns, match_columns)
-print("Values match for the specified columns and rows:", match)
-
-
-#%%
 #add the match id to the project area sample array 
-sarara_sample_matched = np.concatenate((sarara_sample, I), axis = 1)
-
-buffer_file_path = r'C:\Users\35387\OneDrive\Documents\learning\band_subset_buffer.tif'
-dataset = rasterio.open(sarara_file_path)
+#sarara_sample_matched = np.concatenate((sarara_sample, I), axis = 1)
     
-buffer_array[list(I.flatten())]
-
-matching_buffer_coords = np.column_stack((buffer_array[list(I.flatten())], np.array(match_coords_x)))
-matching_buffer_coords = np.column_stack((matching_buffer_coords, np.array(match_coords_y)))
+#matching_buffer_coords = np.column_stack((buffer_array[list(I.flatten())], np.array(match_coords_x)))
+#matching_buffer_coords = np.column_stack((matching_buffer_coords, np.array(match_coords_y)))
 
 #%%
-df_buffer_matches.to_csv('buffer_matches_plot.csv', index = False)
+#df.to_csv('buffer_matches_plot.csv', index = False)
 
 #SENSE CHECKING IN PLOT (geopands package issues so just export and plot outside venv)
 #X = pd.DataFrame(matching_buffer_coords, columns = ['distance_roads','distance_settlements','elevation', 'slope', 'x', 'y'])
