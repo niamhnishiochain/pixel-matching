576e46a96918aa80e460cfe72417a2a1fe581e2e
Niamh Sheehan <sheehan3@tcd.ie>
2023-06-28 18:13:26 +0200

    Update deforestation_history.py
    Works!

--- python_scripts/deforestation_history.py
+++ python_scripts/deforestation_history.py
@@ -6,33 +6,38 @@ Sarara sample vs the matching pixels in the buffer.
 import pandas as pd
 import rasterio
 
-# Read the CSV file containing the coordinates
-csv_file = 'coordinates.csv'
-df = pd.read_csv()
+# Read the CSV files containing the coordinates
+sample_coordinates_filepath = r'C:\Users\35387\OneDrive\Documents\learning\sample_matches_plot.csv'
+buffer_match_coordinates_filepath = r'C:\Users\35387\OneDrive\Documents\learning\buffer_matches_plot.csv'
+sample_coordinates = pd.read_csv(sample_coordinates_filepath)
+buffer_match_coordinates = pd.read_csv(buffer_match_coordinates_filepath)
+buffer_match_coordinates.columns = ['x', 'y']
+# Load the raster files containing the binary deforestation data
+sarara_hansen_filepath = r'C:\Users\35387\OneDrive\Documents\learning\earth_engine_export_data\hansen\export_hansen_data_sarara.tif'
+buffer_hansen_filepath = r'C:\Users\35387\OneDrive\Documents\learning\earth_engine_export_data\hansen\export_hansen_data_buffer.tif'
+sarara_hansen = rasterio.open(sarara_hansen_filepath)
+buffer_hansen = rasterio.open(buffer_hansen_filepath)
 
-# Load the raster file
-raster_file = 'raster.tif'
-raster = rasterio.open(raster_file)
+def calc_deforestation_history(coordinate_df, def_raster):
+    # Initialize the sum variable
+    deforestation_sum = 0
+    # Iterate over the rows in the CSV file
+    for index, row in coordinate_df.iterrows():
+        # Get the coordinates from the CSV file
+        x = row['x']
+        y = row['y']
+        
+        # Convert the coordinates to pixel coordinates
+        row, col = def_raster.index(x, y)
+        
+        # Read the pixel value from the raster
+        pixel_value = def_raster.read(1, window=((row, row+1), (col, col+1)))
+        
+        # Accumulate the pixel value if it's 1
+        if pixel_value == 1:
+            deforestation_sum += pixel_value
 
-# Initialize the sum variable
-pixel_sum = 0
-
-# Iterate over the rows in the CSV file
-for index, row in df.iterrows():
-    # Get the coordinates from the CSV file
-    x = row['x_coordinate']
-    y = row['y_coordinate']
-    
-    # Convert the coordinates to pixel coordinates
-    row, col = raster.index(x, y)
-    
-    # Read the pixel value from the raster
-    pixel_value = raster.read(1, window=((row, row+1), (col, col+1)))
-    
-    # Accumulate the pixel value if it's 1
-    if pixel_value == 1:
-        pixel_sum += pixel_value
-
-# Print the sum of pixel values
-print("Sum of pixel values:", pixel_sum)
+    # Print the sum of pixel values
+    print("Sum of pixel values:", deforestation_sum)
+    return deforestation_sum
 
