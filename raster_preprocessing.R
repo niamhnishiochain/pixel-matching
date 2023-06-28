c2c0cb7cdbe4031286adccdae55314ceb8246ec9
Niamh Sheehan <sheehan3@tcd.ie>
2023-06-28 15:44:02 +0200

    Code now obsolete

--- raster_preprocessing.R
+++ raster_preprocessing.R
@@ -7,41 +7,51 @@
 library(raster)
 
 # Set working directory
-setwd('C:/Users/35387/OneDrive/Documents/learning/data/earth_engine_exports/buffer')
-      #/buffer')
-buffer = stack('band_subset_buffer.tif')
+setwd('C:/Users/35387/Downloads/earth_engine_export_data/matching')
 
+#Open the Earth Engine GeoTiff exports
+buffer = stack('export_matching_data_buffer.tif')
+sarara = stack('export_matching_data_sarara.tif')
+
+################################################################################
 #Merging
+#    This is not necessary with the current earth engine exports
+#     if the files are larger when we add more features this may become 
+#     necessary again.
 # Read input raster files and merge them
 #THIS IS MORE EFFICIENT CODE BUT IT TAKES LONGER TO RUN THAN FROM LINE 19
     #raster_files <- c("1.tif", "2.tif", "3.tif", "4.tif")
     #rasters <- lapply(raster_files, stack)
     #total <- do.call(merge, rasters)
+# one <- stack("1.tif")
+# two <- stack("2.tif")
+# three <- stack("3.tif")
+# four <- stack("4.tif")
+# #Merge step by step to avoid memory errors
+# one_two <- merge(one, two)
+# one_two_three <- merge(one_two, three)
+# total <- merge(one_two_three, four)
+# 
+# # Export the merged file 
+# output_file <- "merged.tif"
+# writeRaster(total, output_file)
 
-one <- stack("1.tif")
-two <- stack("2.tif")
-three <- stack("3.tif")
-four <- stack("4.tif")
-
-#Merge step by step to avoid memory errors
-one_two <- merge(one, two)
-one_two_three <- merge(one_two, three)
-total <- merge(one_two_three, four)
-
-# Export the merged file 
-output_file <- "merged.tif"
-writeRaster(total, output_file)
-
+###############################
 #Subset the bands
+#    Also not now in use!
 #either use the total raster which you create above or read in the file
-total <- stack('merged.tif')
+#total <- stack('merged.tif')
 
 #only keep bands we are matching on for now (which have no missings)
   #distance to roads, distance to settlements, slope, elevation
-buffer_subset <- dropLayer(total, c(1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 15, 16, 17))
-writeRaster(buffer_subset, 'band_subset_buffer_nan.tif', overwrite = TRUE, NAflag = -999)
+#buffer_subset <- dropLayer(total, c(1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 15, 16, 17))
+#writeRaster(buffer_subset, 'band_subset_buffer_nan.tif', overwrite = TRUE, NAflag = -999)
+################################################################################
 
 #Sense Checking Missingness
-#plot(buffer[[2]], colNA = 'red')
-#road = buffer[[1]]
-#table(is.na(road[]))
+band <- 6 #select a band from 1-8
+stack <- sarara #select either 'buffer' or 'sarara'
+print(stack[[band]])
+to_plot <- stack[[band]] #select a band from one of the stacks
+plot(to_plot, colNA = 'red')
+table(is.na(to_plot[]))
