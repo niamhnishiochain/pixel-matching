356cd200ae70f4efa15a3b4a24e4c8e53373143e
Niamh Sheehan <sheehan3@tcd.ie>
2023-06-29 16:51:06 +0200

    Making a start

---
+++ python_scripts/matching_evaluation.py
@@ -0,0 +1,7 @@
+"""
+This script is to evaluate the matching performance.
+Initial idea is to compute the covariate means (and/or
+other relevant statistics) for both treated and control samples.
+@author: Niamh
+"""
+
